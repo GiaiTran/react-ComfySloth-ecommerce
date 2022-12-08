647a710f022da8e8d3a1f15cff05ce4533002b82
GiaiTran <39039933+GiaiTran@users.noreply.github.com>
2022-12-07 23:47:45 -0600

    first commit

---
+++ src/components/Stars.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+const Stars = ({ stars, reviews }) => {
+  return (
+    <Wrapper>
+      <div className='stars'>
+        {/* star */}
+        <span>{stars >= 1 ? <BsStarFill /> : null}</span>
+        {/* END star */}
+      </div>
+      <p className='reviews'>({reviews} customer reviews)</p>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    color: #ffb900;
+    font-size: 1rem;
+    margin-right: 0.25rem;
+  }
+  p {
+    margin-left: 0.5rem;
+    margin-bottom: 0;
+  }
+  margin-bottom: 0.5rem;
+`;
+export default Stars;
